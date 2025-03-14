bd227ecdc3853966c6572bb9654dfa28715eaace
Jorge Raúl Lu Hernández <Jorge.Lu.H@hotmail.com>
2025-03-14 12:00:03 -0600

    Actualización de paquetes.

--- src/Banca.Sesion.Asp/Properties/Settings.Designer.cs
+++ src/Banca.Sesion.Asp/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace Banca.Sesion.Asp.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.11.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.13.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -80,9 +80,9 @@ namespace Banca.Sesion.Asp.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("None")]
-        public string CookieEnlaceMismoSitio {
+        public global::System.Web.SameSiteMode CookieEnlaceMismoSitio {
             get {
-                return ((string)(this["CookieEnlaceMismoSitio"]));
+                return ((global::System.Web.SameSiteMode)(this["CookieEnlaceMismoSitio"]));
             }
         }
     }
